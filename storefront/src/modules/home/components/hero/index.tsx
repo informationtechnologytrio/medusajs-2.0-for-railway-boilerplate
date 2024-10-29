3c24be6411a5536f2124cc864ee74ee5fa549b3d
Anson Chung <hi@anscg.net>
2024-10-29 16:47:51 +0800

    t

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -1,12 +1,13 @@
 import { Github } from "@medusajs/icons"
 import { Button, Heading } from "@medusajs/ui"
+import Image from 'next/image'
 
 const Hero = () => {
   return (
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
         <a href="https://storefront.anscg.net/hk/products/envision">
-          <img src="https://cloud-fhwtc10s1-hack-club-bot.vercel.app/0frame_21.png" className="w-full" alt="Tailwind Play" />
+          <Image src="https://cloud-fhwtc10s1-hack-club-bot.vercel.app/0frame_21.png" className="w-full" alt="Tailwind Play" />
         </a>
       </div>
     </div>
